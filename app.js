906ca1484966072fc383b212b0837302d060e081
natalie sinner <nataliesinner@hotmail.com>
2020-02-22 08:47:08 -0800

    completed chartjs

--- app.js
+++ app.js
@@ -121,43 +121,23 @@ function chartDisplay(){
       datasets: [{
         label: '# of Clicks',
         data: numberArray,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
+        backgroundColor:
+          'rgba(255, 99, 132, 0.2)'
+        ,
+        borderColor:
+          'rgba(255, 99, 132, 1)'
+        ,
         borderWidth: 1
       },
-        {
+      {
         label: '# of Views',
         data: numberViewed,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
+        backgroundColor:
+          'rgba(54, 162, 235, 0.2)'
+        ,
+        borderColor:
+          'rgba(54, 162, 235, 1)'
+        ,
         borderWidth: 1
       }]
     },
